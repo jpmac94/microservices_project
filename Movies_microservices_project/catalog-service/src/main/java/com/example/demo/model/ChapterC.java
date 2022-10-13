e538a3c6c13f9f51526f086a4ecb0b04eab5431c
Julian Mac Allister <jpmacallister94@hotmail.com>
2022-10-13 17:24:21 -0300

    eliminando contenido obsoleto

--- Movies_microservices_project/catalog-service/src/main/java/com/example/demo/model/ChapterC.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model;
-
-public class ChapterC {
-
-    private Integer id;
-    private String name;
-    private String number;
-    private String urlStream;
-}
--- Movies_microservices_project/catalog-service/src/main/java/com/example/demo/model/SeasonC.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.model;
-
-public class SeasonC {
-
-    private Integer id;
-    private Integer seasonNum;
-    private ChapterC chapter;
-}
